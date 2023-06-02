989a1ae3b4c4eec88f17aafaf300e45f4570f537
Mark Stock <markjstock@gmail.com>
2023-06-02 10:21:37 -0400

    Renamed some arrays in preparation for supporting arbitrary levels of hierarchy

--- switchboard.cpp
+++ switchboard.cpp
@@ -34,9 +34,9 @@ struct machine_t {
 // data for frontier
 const std::string machname = "frontier";
 const int node_levels = 2;
-const int node_hierarchy[node_levels] = {128, 74};
+const int num_per_level[node_levels] = {128, 74};
 const int num_per_row[node_levels] = {8, 15};
-const int total_nodes = 9472;
+const int total_num[node_levels] = {9472, 74};
 // map the name of the machine to a 0-indexed, continuous index
 int map_node_name(const int _n) {
   if (_n <= 9088) return _n-1;
@@ -48,9 +48,9 @@ int map_node_name(const int _n) {
 // data for crusher
 const std::string machname = "crusher";
 const int node_levels = 2;
-const int node_hierarchy[node_levels] = {128, 2};
+const int num_per_level[node_levels] = {128, 2};
 const int num_per_row[node_levels] = {8, 2};
-const int total_nodes = 192;
+const int total_num[node_levels] = {192, 2};
 // map the name of the machine to a 0-indexed, continuous index
 int map_node_name(const int _n) {
   if (_n <= 192) return _n-1;
@@ -82,7 +82,9 @@ int main(int argc, char *argv[])
   std::string nodelist(std::istreambuf_iterator<char>{ifs}, {});
   ifs.close();
 
-  // create new image data arrays
+  // --------------------------------------------------------------------------
+  // create arrays for the geometric hierarchy
+
   int boxszx[node_levels+1];
   int boxszy[node_levels+1];
   int boxbdr[node_levels+1];
@@ -99,50 +101,54 @@ int main(int argc, char *argv[])
   boxhgt[0] = boxgap[0] + 2*boxbdr[0] + boxszy[0];
   // drawing sizes per block
   boxszx[1] = num_per_row[0]*boxwid[0];
-  boxszy[1] = rows_needed(node_hierarchy[0],num_per_row[0])*boxhgt[0];
-  boxbdr[1] = 0;
+  boxszy[1] = rows_needed(num_per_level[0],num_per_row[0])*boxhgt[0];
+  boxbdr[1] = 1;
   boxgap[1] = boxwid[0];
   boxwid[1] = boxgap[1] + 2*boxbdr[1] + boxszx[1];
   boxhgt[1] = boxgap[1] + 2*boxbdr[1] + boxszy[1];
   // and for the whole image
   boxszx[2] = num_per_row[1]*boxwid[1];
-  boxszy[2] = rows_needed(node_hierarchy[1],num_per_row[1])*boxhgt[1];
-  boxbdr[2] = 0;
+  boxszy[2] = rows_needed(num_per_level[1],num_per_row[1])*boxhgt[1];
+  boxbdr[2] = 1;
   boxgap[2] = 2*boxgap[1];
   boxwid[2] = boxgap[2] + 2*boxbdr[2] + boxszx[2];
   boxhgt[2] = boxgap[2] + 2*boxbdr[2] + boxszy[2];
+
+  // --------------------------------------------------------------------------
+  // clear the image and set the background
+
   unsigned int out_width = boxwid[2];
   unsigned int out_height = boxhgt[2];
   printf("Will create %d x %d image\n", out_width, out_height);
-
-  unsigned char bgcolor[4] = {255, 255, 255, 255};
-
   std::vector<unsigned char> out_image;
   out_image.resize(out_width * out_height * 4);
+
   // fill with solid white
+  const unsigned char bgcolor[4] = {255, 255, 255, 255};
   for (unsigned int i = 0; i < out_width*out_height; i++) {
     out_image[4*i+0] = bgcolor[0];
     out_image[4*i+1] = bgcolor[1];
     out_image[4*i+2] = bgcolor[2];
     out_image[4*i+3] = bgcolor[3];
-    //out_image[4*i+3] = 0;
   }
 
-  int nnodes = 1;
-  for (int i=0; i<node_levels; ++i) nnodes *= node_hierarchy[i];
-  if (nnodes > total_nodes) nnodes = total_nodes;
-  std::cout << "Drawing outlines for " << nnodes << " nodes..." << std::endl;
+  // --------------------------------------------------------------------------
+  // march through all levels and draw their boxes
 
-  unsigned char bdrcolor[4] = {192, 192, 192, 255};
+  const unsigned char bdrcolor[4] = {192, 192, 192, 255};
 
-  // march through all nodes and draw their boxes
-  if (boxbdr[0] > 0) {
+  //for (int i=0; i<=node_levels; ++i) {
+  for (int i=0; i<=0; ++i) {
+    std::cout << "Drawing outlines for " << total_num[i] << " blocks..." << std::endl;
+
+    if (boxbdr[i] > 0) {
+
     // can add openmp here if necessary
-    for (int cnt = 0; cnt < nnodes; cnt++) {
-      const int group = cnt / node_hierarchy[0];
+    for (int cnt = 0; cnt < total_num[i]; cnt++) {
+      const int group = cnt / num_per_level[0];
       const int igroup = group % num_per_row[1];
       const int jgroup = group / num_per_row[1];
-      const int node = cnt - group*node_hierarchy[0];
+      const int node = cnt - group*num_per_level[0];
       const int inode = node % num_per_row[0];
       const int jnode = node / num_per_row[0];
 
@@ -183,15 +189,9 @@ int main(int argc, char *argv[])
       }
     }
   }
-
-  // march through all groups and draw their borders
-  if (boxbdr[1] > 0) {
   }
 
-  // march through all higher level groups and draw their borders
-  if (boxbdr[2] > 0) {
-  }
-
+  // --------------------------------------------------------------------------
   // repeatedly look for the keyword in the nodelist string and generate jobs
   std::vector<job_t> jobs;
 
@@ -273,12 +273,12 @@ int main(int argc, char *argv[])
     jobs.push_back(newjob);
   }
 
-  // sort jobs from long to short
+  // --------------------------------------------------------------------------
+  // march through active jobs and draw them
 
   // set drawing parameters
   const bool overwrite_border = true;
 
-  // march through active jobs and draw them
   std::cout << "Drawing active nodes..." << std::endl;
   for (auto job : jobs) {
 
@@ -294,10 +294,10 @@ int main(int argc, char *argv[])
 
       if (nodeidx < 0) continue;
 
-      const int group = nodeidx / node_hierarchy[0];
+      const int group = nodeidx / num_per_level[0];
       const int igroup = group % num_per_row[1];
       const int jgroup = group / num_per_row[1];
-      const int node = nodeidx - group*node_hierarchy[0];
+      const int node = nodeidx - group*num_per_level[0];
       const int inode = node % num_per_row[0];
       const int jnode = node / num_per_row[0];
 
@@ -321,7 +321,9 @@ int main(int argc, char *argv[])
     }
   }
 
+  // --------------------------------------------------------------------------
   // output to a new png
+
   unsigned int error = lodepng::encode(out_file, out_image, out_width, out_height);
   //if there's an error, display it
   if (error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
